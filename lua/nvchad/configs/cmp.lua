8c4115c4c35fa1254152f2926cccae8e60611282
Lucaswatt <140810532+Lucaswatt@users.noreply.github.com>
2024-03-20 17:44:18 +1300

    Update cmp.lua
    Change intellisense complete keys because the default one was enter and it is SO ANNOYING OMG

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -73,15 +73,14 @@ local options = {
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
 
-    ["<CR>"] = cmp.mapping.confirm {
+    ["<Tab>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-Space>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif require("luasnip").expand_or_jumpable() then
